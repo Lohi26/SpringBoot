d21d6e6d7187689e8ba63046e910e23108e20759
Lohi26 <lohitachandru@gmail.com>
2024-04-07 16:10:04 +0530

    Last Commit for Database Connection of a form

--- SpringBootApplication/src/main/java/com/example/demo/controller/homeController.java
+++ SpringBootApplication/src/main/java/com/example/demo/controller/homeController.java
@@ -3,6 +3,8 @@ package com.example.demo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.example.demo.newClass.Nothing;
 import com.example.demo.repo.nothingRepo;
@@ -27,4 +29,5 @@ public class homeController {
 		nothingrep.save(nothing);
 		return "home";
 	}
+	
 }
